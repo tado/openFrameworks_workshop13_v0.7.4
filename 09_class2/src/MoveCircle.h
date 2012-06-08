316d14a1dc5c9860d033affe147b02e8a65d9bf0
Atsushi Tadokoro <tadokoro@gmail.com>
2012-06-09 07:19:59 +0900

    initial commit

---
+++ 09_class2/src/MoveCircle.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "ofMain.h"
+
+class MoveCircle {
+	
+public:
+	
+	void draw();
+	void update();
+	ofPoint interpolateByPct(float pct);
+	
+	ofPoint currentPos;
+	ofPoint startPos;
+	ofPoint endPos;
+	float pct;
+	float shaper;
+};
